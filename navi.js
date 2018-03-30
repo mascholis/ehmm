a0f37bbe54e0afe0b5342f2c1ffaf141e6bc97b5
mascholis <colcholiz@gmail.com>
2018-03-30 10:53:22 +0700

    Create navi.js

---
+++ navi.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#creditmc,.creditmc").attr("href") != "http://www.tipsim.com") {
+        window.location.href = "http://www.tipsim.com"; }});
+//]]>
